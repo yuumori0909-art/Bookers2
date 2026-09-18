3a02621e0b5d04e75c0793d7d9d2c3cd119df219
Ubuntu <ubuntu@ip-172-31-18-99.ap-northeast-1.compute.internal>
2026-09-18 13:22:38 +0000

    [start] Bookers2

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  allow_unauthenticated_access only: [:new, :create] 
+ 
+  def new
+    @user = User.new
+  end
+ 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # ユーザー登録成功後、ログイン画面へリダイレクト
+      redirect_to new_session_path, notice: "ユーザー登録が完了しました！続けてログインしてください。"
+    else
+      # エラー時はフォームを再表示
+      render :new, status: :unprocessable_entity
+    end
+  end
+ 
+  private
+ 
+  def user_params
+    params.require(:user).permit(:name, :email_address, :password, :password_confirmation)
+  end
+end
